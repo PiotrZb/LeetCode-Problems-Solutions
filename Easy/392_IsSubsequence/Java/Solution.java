e72650bdec9405687564c60fb39489bd563b806c
PiotrZb <piotr.zborowski2000@gmail.com>
2023-09-22 13:29:47 +0200

    Problem 392 - java

---
+++ Easy/392_IsSubsequence/Java/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) return true;
+
+        int sPointer = 0;
+        
+        for (int tPointer = 0; tPointer < t.length(); tPointer++) {
+            if (t.charAt(tPointer) == s.charAt(sPointer)) {
+                 sPointer++;
+                 if (sPointer == s.length()) return true;
+            }
+        }
+        
+        return false;
+    }
+}
