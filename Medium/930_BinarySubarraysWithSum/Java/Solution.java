b6101bbeff88d51d48a7a424d8fd442a14f73c45
PiotrZb <piotr.zborowski2000@gmail.com>
2024-03-14 12:51:12 +0100

    Problem 930

---
+++ Medium/930_BinarySubarraysWithSum/Java/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int answer = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == goal) answer++;
+                else if (sum > goal) break;
+            }
+        }
+
+        return answer;
+    }
+}
