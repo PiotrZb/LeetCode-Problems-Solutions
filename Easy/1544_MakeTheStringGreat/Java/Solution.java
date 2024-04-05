84b191273949f07eef4ee3406bed5bc2efa98df3
PiotrZb <piotr.zborowski2000@gmail.com>
2024-04-05 15:36:21 +0200

    Problem 1544

---
+++ Easy/1544_MakeTheStringGreat/Java/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String makeGood(String s) {
+        StringBuilder sb = new StringBuilder(s.length());
+
+        for (char c : s.toCharArray()) {
+            if (!sb.isEmpty() && Math.abs(sb.charAt(sb.length() - 1) - c) == 32) sb.deleteCharAt(sb.length() - 1);
+            else sb.append(c);
+        }
+
+        return sb.toString();
+    }
+}
