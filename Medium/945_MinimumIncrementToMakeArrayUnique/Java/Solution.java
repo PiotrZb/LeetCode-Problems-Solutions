b8a4e563c553588eee9a6be82e24f3166e108474
PiotrZb <piotr.zborowski2000@gmail.com>
2024-06-14 13:21:59 +0200

    Problem 945

---
+++ Medium/945_MinimumIncrementToMakeArrayUnique/Java/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+        int res = 0;
+
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                res += nums[i - 1] - nums[i] + 1;
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        
+        return res;
+    }
+}
