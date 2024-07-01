f351ee55492f214cf06b9fc25d442af84a65a703
PiotrZb <piotr.zborowski2000@gmail.com>
2024-07-01 15:35:18 +0200

    Problem 1550

---
+++ Easy/1550_ThreeConsecutiveOdds/JavaScript/Solution.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+    for (let i = 0; i < arr.length - 2; i++) {
+        if (arr[i] % 2 && arr[++i] % 2 && arr[++i] % 2) return true;
+    }
+
+    return false;
+};
