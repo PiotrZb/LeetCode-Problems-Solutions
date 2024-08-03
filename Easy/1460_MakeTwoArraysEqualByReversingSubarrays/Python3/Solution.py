5b1940976bd06aa9ad9660f5b5c0d39ec36a952f
PiotrZb <piotr.zborowski2000@gmail.com>
2024-08-03 15:38:02 +0200

    Problem 1460

---
+++ Easy/1460_MakeTwoArraysEqualByReversingSubarrays/Python3/Solution.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+class Solution:
+    def canBeEqual(self, target: list[int], arr: list[int]) -> bool:
+        return Counter(target) == Counter(arr)
+        # return sorted(target) == sorted(arr)
